3a104161703e55cdc27ae68af3dd645209ebbee9
한재호 <wogh009@gmail.com>
2025-10-01 11:16:11 +0900

    supabase key

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 // es-module -> import, commonjs -> require
-
-const express = require("express"); //express 안에 이미 구현되어있는 코드들을 express 객체 형태로 불러오는 것
-
-const app = express(); //express() -> app 객체 생성
-const PORT = 3000;
+const express = require("express"); // express 안에 있는 이미 구현되어 있는 코드들을 express 객체 형태로 불러오겠다
 const cors = require("cors");
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-}); //서버 오픈
-app.get("/", (req, res) => {
-  res.send("Hello World"); //응답
-}); //get -> 요청, '/' -> 경로, req -> 요청정보, res -> 응답정보
+const app = express();
+const port = 3000;
+const dotenv = require("dotenv");
+const { createClient } = require("@supabase/supabase-js");
+dotenv.config();
 
+const { SUPABASE_KEY: supabasekey, SUPABASE_URL: supabaseurl } = process.env;
+console.log("SUPABSE KEY : ", supabase);
+console.log("SUPABSE URL : ", supabaseurl);
+const supabase = createClient(supabaseurl, supabasekey);
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
+});
+
+app.get("/", (req, res) => {
+  // req -> request -> 전달 받은 데이터나 요청사항
+  // res -> response -> 응답할 내용/방식을 담은 객체
+  res.send("hello");
+});
+
+// DOM listener / server '대기' -> 특정한 요청. -> 응답.
